bb3142f1ee2f5fef2fdbfc36aecd2e6b35e7ff26
Ramón Ortiz Castañeda <joseramonortizcastaneda@MacBook-Pro-de-Ramon.local>
2023-09-08 14:54:37 -0600

    First E.

---
+++ P01/EX01.m
@@ -0,0 +1,17 @@
+% Imagen original (1024 x 1024) en escala de grises.
+originalImage = im2gray(imread('CarlosIV1024.jpg'));
+newDimentions = 1024; % (512, 256, 128 o 64)
+newImage = reduce(originalImage,newDimentions);
+imshow(newImage); 
+
+% La función recibe como parámetros la imagen original y una 
+% variable que almacena la dimension de la nueva imagen reducida.  
+function newImage = reduce(oldImage,newScale)
+    aux = length(oldImage)/newScale;
+    newImage = zeros(newScale,newScale,'uint8');
+    for i = 1:newScale
+        for j = 1:newScale
+        newImage(i, j) = oldImage(aux * i, aux * j);
+        end
+    end
+end
